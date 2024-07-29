6e54d669e2b854d7ebfa5c50e041fdd3cc47e625
BrianBSP <167891999+BrianBSP@users.noreply.github.com>
2024-07-29 16:11:07 +0200

    aggiunto esercizio 2

--- src/es1/Main.java
+++ src/es1/Main.java
@@ -1,7 +1,38 @@
 package es1;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static <input> void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = new int[5];
+        Random random = new Random();
+        numbers[0] = random.nextInt(1, 10);
+        numbers[1] = random.nextInt(1, 10);
+        numbers[2] = random.nextInt(1, 10);
+        numbers[3] = random.nextInt(1, 10);
+        numbers[4] = random.nextInt(1, 10);
+
+        System.out.println(Arrays.toString(numbers));
+
+        while (true) {
+            System.out.println("Inserisci un numero da mettere all'interno dell'array. Il numero deve essere compreso da 1 e 10.");
+            int input = Integer.parseInt(scanner.nextLine());
+
+            if (input == 0) {
+                System.out.println("Hai spento l'applicazione!!");
+                break;
+            } else if (input > 0 && input <= 10) {
+                numbers[4] = input;
+                System.out.println(Arrays.toString(numbers));
+            } else {
+                System.out.println("Attenzione!! Inserisci un numero compreso tra 1 e 10");
+            }
+        }
 
     }
+
 }
--- src/es2/Main.java
+++ src/es2/Main.java
@@ -1,7 +1,25 @@
 package es2;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("-------- km ---------");
+        System.out.println("Inserisci i km percorsi:");
+        int km = Integer.parseInt(scanner.nextLine());
+        System.out.println("------- gasolio --------");
+        System.out.println("Inserisci i litri di gasolio consumati:");
+        int gasolioConusmato = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("------- km/litri --------");
+        System.out.println("I km percorsi ogni litro di carburante sono: ");
+        System.out.println(kmAlLitro(km, gasolioConusmato));
+
+    }
+
+    public static int kmAlLitro(int km, int litriConsumati) {
+        return km / litriConsumati;
     }
 }
